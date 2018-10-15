8005a2b1af6b66c9f7d69c8dc3faf6d720b5c6ca
Alexis Kelly <alexis.kelly@bjss.com>
2018-10-15 20:07:36 +0100

    first_most_basic_test_ran_with_gradle

---
+++ src/test/java/NewJavaClassTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by alexis.kelly on 15-Oct-18.
+ */
+public class NewJavaClassTest {
+
+    public static void main(String args[]){
+        System.out.println("This test worked successfully!!!");
+    }
+}
